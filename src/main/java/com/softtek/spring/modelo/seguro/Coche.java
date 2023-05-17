bf06677d285be8a50fe0cdee0bed1440f526632b
IsraelSerrano1 <israel.serrano@softtek.com>
2023-05-17 11:17:12 +0200

    Implementacion de lombok

--- src/main/java/com/softtek/spring/modelo/seguro/Coche.java
+++ src/main/java/com/softtek/spring/modelo/seguro/Coche.java
@@ -1,26 +1,14 @@
 package com.softtek.spring.modelo.seguro;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@EqualsAndHashCode
 public class Coche {
     private String matricula;
     private String modelo;
 
-    public Coche(String matricula, String modelo) {
-        this.matricula = matricula;
-        this.modelo = modelo;
-    }
-    public Coche() {
-    }
-
-    public String getMatricula() {
-        return matricula;
-    }
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-    public String getModelo() {
-        return modelo;
-    }
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
 }
